ad027ddccbfefa42f6c56d256b48212268d33e7a
Colina Guan <cobguan@ucsc.edu>
2019-08-08 15:28:03 -0400

    Minor home page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,20 +9,22 @@ class Home extends Component {
                 <div className="home-banner">
                     <div className="banner-cover" />
                     <img className="banner-img" src={bannerImg} />
-                    <h3 className="banner-header">Cedar</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id.</p>
+                    <div className="container banner-header">
+                        <h3>Cedar</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id.</p>
+                    </div>
                 </div>
                 <div className="container sign-up">
                     <div className="sign-up-opt row">
                         <div className="start-borrow col">
                             <h4>Start Borrowing</h4>
                             <p>Info about borrowing</p>
-                            <button type="button" className="btn btn-info">Sign Up</button>
+                            <a href="/chatbot" role="button" className="btn btn-info">Get Started</a>
                         </div>
                         <div className="start-lend col">
                             <h4>Start Lending</h4>
                             <p>Info about lending</p>
-                            <button type="button" className="btn btn-info">Sign Up</button>
+                            <a href="/signup" role="button" className="btn btn-info">Get Started</a>
                         </div>
                     </div>
                 </div>
