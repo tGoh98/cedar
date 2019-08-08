9effd8469b033ed10c816b124396032f3e257e69
Colina Guan <cobguan@ucsc.edu>
2019-08-08 15:11:19 -0400

    Edited home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,26 @@ import bannerImg from '../images/banner.jpg'
 class Home extends Component {
     render() {
         return (
-            <div className="container">
-                home
+            <div>
                 <div className="home-banner">
+                    <div className="banner-cover" />
                     <img className="banner-img" src={bannerImg} />
+                    <h3 className="banner-header">Cedar</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id.</p>
+                </div>
+                <div className="container sign-up">
+                    <div className="sign-up-opt row">
+                        <div className="start-borrow col">
+                            <h4>Start Borrowing</h4>
+                            <p>Info about borrowing</p>
+                            <button type="button" className="btn btn-info">Sign Up</button>
+                        </div>
+                        <div className="start-lend col">
+                            <h4>Start Lending</h4>
+                            <p>Info about lending</p>
+                            <button type="button" className="btn btn-info">Sign Up</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
