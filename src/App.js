5ce69243c35aa48e43c9e2d2b3b15292344a8c60
tGoh98 <tmg5@rice.edu>
2019-08-08 14:53:56 -0400

    Added router transitions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Switch, Route, withRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 import './App.css';
 import Home from './components/Home';
