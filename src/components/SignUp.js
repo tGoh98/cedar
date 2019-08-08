2548fbb859e5f2a9f8724bbcccc49e1f3ff44ad6
Colina Guan <cobguan@ucsc.edu>
2019-08-08 15:47:05 -0400

    Started sign-up page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            region: "",
+            industry: "",
+            contact: {
+                email: "",
+                phone: "",
+                website: ""
+            },
+            info: "",
+            tags: []
+        }
+
+
+    }
     render() {
         return (
             <div>
-              signup
+                signup
             </div>
         );
     }
